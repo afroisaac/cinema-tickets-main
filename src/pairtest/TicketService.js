0e596cab8bc2d82be92cb970ee54dbf6f52b92cc
Isaac Awonugba <78800584+afroisaac@users.noreply.github.com>
2023-02-11 12:17:45 +0000

    Update

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -64,6 +64,10 @@ export default class TicketService {
 }
 
 // Sample implementation of the TicketService Class. The can be imported in another file and this implementation do in that separate file
-let ticketService = new TicketService();
-const request = [new TicketTypeRequest("ADULT", 2), new TicketTypeRequest("CHILD", 4)];
-ticketService.purchaseTickets(1, ...request);
+try {
+    let ticketService = new TicketService();
+    const request = [new TicketTypeRequest("ADULT", 2), new TicketTypeRequest("CHILD", 4)];
+    ticketService.purchaseTickets(1, ...request);
+} catch (ex) {
+    //    console.log(ex);
+}
